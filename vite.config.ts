f42fe2afb9940310d9901ce0ffb22a4ed7ffea5a
abdullahkamal321-admin <kamaludeenabdullahi123@gmail.com>
2026-06-03 16:55:42 +0100

    fix base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/my-PortFolio/'
+  //base: '/my-PortFolio/'
+  base: '/'
 })
